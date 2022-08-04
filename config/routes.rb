0353cb6f95682408d53cdd86efb726bf805ed9a0
Norman.Ablaza <namronazalba@gmail.com>
2022-08-04 17:43:02 +0800

    add view and update page to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   constraints(ClientDomainConstraint.new) do
-    devise_for :users
+    devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
     root :to => 'home#index'
+    get 'profile', to: 'home#me'
   end
 
   constraints(AdminDomainConstraint.new) do

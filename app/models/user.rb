5e749073348735f3d5dab9c3fd4c7019460a0a3a
Norman.Ablaza <namronazalba@gmail.com>
2022-08-05 16:24:30 +0800

    install phonelib gem to validate by phone number for philippines

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
 
+  validates :phone, phone: true
+
   mount_uploader :avatar, AvatarUploader
+
+  def client?
+    role == 'client'
+  end
+
+  def admin?
+    role == 'admin'
+  end
+
 end

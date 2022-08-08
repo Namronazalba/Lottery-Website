607d109b76bcc177e8dc12fd0dad0f907d2fd75b
Norman.Ablaza <namronazalba@gmail.com>
2022-08-08 11:14:55 +0800

    user can update info(except password) without current password and required current password if password need to update

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,22 +1,38 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
-
 
   def edit
-    super
+    @user = current_user
   end
 
-  # PUT /resource
   def update
-    super
+    if params[:user][:current_password].present?
+      if current_user.update_with_password(password_params)
+        flash[:alert]="Updated successfully!"
+        sign_in @user, :bypass => true
+        redirect_to edit_user_registration_path
+      else
+        flash[:error] = 'Oh No! Something Went Wrong in password update field, Please Try Again.'
+        render :edit
+      end
+    else
+      if current_user.update(userinfo_params)
+        flash[:alert]="Updated successfully!"
+        redirect_to edit_user_registration_path
+      else
+        flash[:error] = 'Oh No! Something Went Wrong, Please Try Again.'
+        render :edit
+      end
+    end
   end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:phone,:username,  :avatar])
-  end
+  private
 
+  def userinfo_params
+    params.require(:user).permit(:email, :username, :phone, :avatar)
+  end
+  def password_params
+    params.require(:user).permit(:password,:password_confirmation, :current_password)
+  end
 end

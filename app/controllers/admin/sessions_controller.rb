bc7e6b95452ff181aa626dee8447f32922a85609
Norman.Ablaza <namronazalba@gmail.com>
2022-08-08 11:12:06 +0800

    only admin can login on the admin page

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,3 +1,16 @@
 class Admin::SessionsController < Devise::SessionsController
 
+  # users/sign_in
+  def create
+    user = User.find_by_email(params[:admin_user][:email])
+    if user.nil?
+      flash[:notice] = "account does not exists"
+      redirect_to action: :new
+    elsif user.admin?
+      super
+    else
+      flash[:notice] = "Your account is not admin"
+      redirect_to action: :new
+    end
+  end
 end

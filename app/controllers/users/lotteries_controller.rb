53ef29b6056b0f4d136743fb0f6e443b75da4557
Norman.Ablaza <namronazalba@gmail.com>
2022-08-31 16:30:41 +0800

    [Feature #18]Client can cancel order

--- app/controllers/users/lotteries_controller.rb
+++ app/controllers/users/lotteries_controller.rb
@@ -31,7 +31,7 @@ class Users::LotteriesController < UsersController
           @bet.save!
         end
       end
-      flash[:notice] = "successfully created"
+      flash[:notice] = "Betting successfully! Thanks for betting"
     rescue ActiveRecord::RecordInvalid => exception
       flash[:alert] = exception
     end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   has_many :bets
   validates :image, :name, :quantity, :minimum_bet, :batch_count, :online_at, :offline_at, :start_at, :status, presence: true
   scope :filter_by_category, -> (category) { includes(:category).where(category: {name: category}) }
+  validates :quantity, :minimum_bet, numericality: { greater_than: 0 }
 
   enum status: [:active, :inactive]
 
@@ -20,7 +21,7 @@ class Item < ApplicationRecord
     state :starting, :paused, :ended, :cancelled
 
     event :start do
-      transitions from: [:pending, :ended, :cancelled], to: :starting, after: :change_status, guards: [ :less_than_one?, :offline_time?, :active?]
+      transitions from: [:pending, :ended, :cancelled], to: :starting, after: :change_status, guards: [:less_than_one?, :offline_time?, :active?]
       transitions from: :paused, to: :starting
     end
 
@@ -29,16 +30,34 @@ class Item < ApplicationRecord
     end
 
     event :end do
-      transitions from: :starting, to: :ended
+      transitions from: [:starting, :paused], to: :ended,after: :select_winner,guards: :min_bet?
     end
 
     event :cancel do
-      transitions from: [:starting, :paused], to: :cancelled
+      transitions from: [:starting, :paused], to: :cancelled, after: [:if_cancel?, :return_coin]
     end
+  end
+
+  def select_winner
+    select_one = bets.where(batch_count: batch_count).where.not(state: :cancelled)
+    selected_winner = select_one.sample
+    selected_winner.win!
+    select_one.where.not(id: selected_winner.id).update_all(state: :lost)
+
+    winner = Winner.new( user: selected_winner.user, bet: selected_winner, item: selected_winner.item, item_batch_count: selected_winner.batch_count, address: selected_winner.user.addresses.find_by(is_default: true))
+    winner.save!
+  end
+
+  def return_coin
+    self.update(quantity: self.quantity + 1, batch_count: self.batch_count - 1)
   end
 
   def change_status
     self.update(quantity: self.quantity - 1, batch_count: self.batch_count + 1)
+  end
+
+  def if_cancel?
+    bets.where(batch_count: batch_count).where.not(state: :cancelled).each { |bet| bet.cancel! }
   end
 
   def less_than_one?
@@ -47,5 +66,13 @@ class Item < ApplicationRecord
 
   def offline_time?
     offline_at > Time.now
+  end
+
+  def min_bet?
+    bets.where(batch_count: batch_count).count >= minimum_bet
+  end
+
+  def pick_winner
+
   end
 end
